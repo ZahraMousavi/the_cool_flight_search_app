81030b00ce63483176fdcb80e71a1520ef4a49f6
zahra <zahramousavidk@gmail.com>
2022-08-16 03:33:19 -0700

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'the_cool_flight_search_app';
+}
