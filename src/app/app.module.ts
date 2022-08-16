3e19d34a807157ee94005c236c9e1f3429408aac
zahra <zahramousavidk@gmail.com>
2022-08-16 07:18:52 -0700

    Added main component to the app, and as default route.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header.component';
 import { FooterComponent } from './shared/footer.component';
-import {MaterialModule} from './material.module'
+import { MaterialModule } from './material.module';
+import { MainComponent } from './main/main.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
