b0374d219f6c072671de8617aaf2d3ca8f559b7a
zahra <zahramousavidk@gmail.com>
2022-08-21 08:32:14 -0700

    Imported some necessary modules.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from "@angular/common/http";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,6 +8,10 @@ import { HeaderComponent } from './shared/header.component';
 import { FooterComponent } from './shared/footer.component';
 import { MaterialModule } from './material.module';
 import { MainComponent } from './main/main.component'
+import { FormsModule } from "@angular/forms";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
+import { MomentDateAdapter } from '@angular/material-moment-adapter';
 
 @NgModule({
   declarations: [
@@ -17,10 +22,26 @@ import { MainComponent } from './main/main.component'
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     MaterialModule,
+    HttpClientModule,
+    FormsModule,
   ],
-  providers: [],
+  providers: [
+    { provide: MAT_DATE_LOCALE, useValue: 'en-GB' },
+    { provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE] },
+    {
+      provide: MAT_DATE_FORMATS, useValue: {
+        parse: {
+          dateInput: 'YYYY/MM/DD', // this is how your date will be parsed from Input
+        },
+        display: {
+          dateInput: 'YYYY/MM/DD', // this is how your date will get displayed on the Input
+        }
+      }
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -10,22 +10,22 @@ import {NgModule} from '@angular/core';
 // import {MatAutocompleteModule} from '@angular/material/autocomplete';
 // import {MatBadgeModule} from '@angular/material/badge';
 // import {MatBottomSheetModule} from '@angular/material/bottom-sheet';
-// import {MatButtonModule} from '@angular/material/button';
-// import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatButtonModule} from '@angular/material/button';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
 // import {MatCardModule} from '@angular/material/card';
 // import {MatCheckboxModule} from '@angular/material/checkbox';
 // import {MatChipsModule} from '@angular/material/chips';
 // import {MatStepperModule} from '@angular/material/stepper';
-// import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatDatepickerModule} from '@angular/material/datepicker';
 // import {MatDialogModule} from '@angular/material/dialog';
 // import {MatDividerModule} from '@angular/material/divider';
 // import {MatExpansionModule} from '@angular/material/expansion';
 // import {MatGridListModule} from '@angular/material/grid-list';
 import {MatIconModule} from '@angular/material/icon';
-// import {MatInputModule} from '@angular/material/input';
+import {MatInputModule} from '@angular/material/input';
 // import {MatListModule} from '@angular/material/list';
 // import {MatMenuModule} from '@angular/material/menu';
-// import {MatNativeDateModule, MatRippleModule} from '@angular/material/core';
+import {MatNativeDateModule, MatRippleModule} from '@angular/material/core';
 // import {MatPaginatorModule} from '@angular/material/paginator';
 // import {MatProgressBarModule} from '@angular/material/progress-bar';
 // import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
@@ -54,22 +54,22 @@ import {MatToolbarModule} from '@angular/material/toolbar';
     // MatAutocompleteModule,
     // MatBadgeModule,
     // MatBottomSheetModule,
-    // MatButtonModule,
-    // MatButtonToggleModule,
+    MatButtonModule,
+    MatButtonToggleModule,
     // MatCardModule,
     // MatCheckboxModule,
     // MatChipsModule,
     // MatStepperModule,
-    // MatDatepickerModule,
+    MatDatepickerModule,
     // MatDialogModule,
     // MatDividerModule,
     // MatExpansionModule,
     // MatGridListModule,
     MatIconModule,
-    // MatInputModule,
+    MatInputModule,
     // MatListModule,
     // MatMenuModule,
-    // MatNativeDateModule,
+    MatNativeDateModule,
     // MatPaginatorModule,
     // MatProgressBarModule,
     // MatProgressSpinnerModule,
