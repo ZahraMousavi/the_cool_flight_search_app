5fa6206192ec8350b8803946db599764136092fb
zahra <zahramousavidk@gmail.com>
2022-08-21 08:33:14 -0700

    Worked on main page.

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,15 +1,124 @@
 import { Component, OnInit } from '@angular/core';
+import { AmadeusService } from '../amadeus.service'
+import {catchError, map, switchMap, tap} from "rxjs/operators";
+import {EMPTY} from "rxjs";
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
 })
-export class MainComponent implements OnInit {
+export class MainComponent{
+  from: any = "";
+  fromLocation: any = [];
+  origin: any;
 
-  constructor() { }
+  to: any = "";
+  toLocation: any = [];
+  destination: any;
 
-  ngOnInit(): void {
+  date: any = "";
+
+  flights: any;
+  flightTemplate: boolean = false;
+
+  booked: boolean = false;
+
+  first: string = "";
+  last: string= "";
+
+  wait: boolean = false;
+
+  constructor(
+    private amadeusService: AmadeusService,
+  ) {
+  }
+
+  getFromLocation = () => {
+    if (this.from.length > 3) {
+      this.amadeusService.getCityAndAirport(this.from).pipe(
+        // @ts-ignore
+        tap(jsonResponse => this.fromLocation = jsonResponse.data),
+        catchError(error => this.handleError(error)),
+      ).subscribe();
+    }
+  };
+
+  getOrigin = (location: any) => {
+    this.origin = location;
+    this.fromLocation = [];
+  };
+
+  getToLocation = () => {
+    if (this.to.length > 3) {
+      this.amadeusService.getCityAndAirport(this.to).pipe(
+        // @ts-ignore
+        tap(jsonResponse => this.toLocation = jsonResponse.data),
+        catchError(error => this.handleError(error)),
+      ).subscribe();
+    }
+  };
+
+  getDestination = (location: any) => {
+    this.destination = location;
+    this.toLocation = []
+  };
+
+
+  onFindFlight = () =>  {
+    if (this.origin?.iataCode === undefined) {
+      alert("Please choose a from airport")
+    }
+    else if (this.destination?.iataCode === undefined) {
+      alert("Please choose a to airport")
+    }
+    else if (this.date === "" || this.date === null) {
+      alert("Please choose a date")
+    }
+    else {
+      this.wait = true;
+      console.log('please wait', this.date);
+      this.amadeusService.getFlights(this.origin.iataCode, this.destination.iataCode, this.date.format("YYYY-MM-DD")).pipe(
+        tap(() => this.wait = false),
+        tap(() => console.log('end waiting')),
+        // @ts-ignore
+        tap(jsonResponse => this.flights = jsonResponse.data),
+        tap(() => this.flightTemplate = true),
+        catchError(error => this.handleError(error)),
+      ).subscribe();
+    }
+
+  };
+
+  onBookFlight = (flight: any) => {
+
+    if (this.first === "" && this.last === "") {
+      alert("Enter your first and last name");
+      return;
+    }
+
+    const data = { flight: flight };
+    const name = {
+      first: this.first,
+      last: this.last
+    };
+    const dataForBookingFlight = { flight: flight, name: name };
+    this.amadeusService.confirmFlight(data).pipe(
+      catchError(error => this.handleError(error)),
+      tap(res => console.log('Success:', res)),
+      switchMap(()=>this.amadeusService.bookFlight(dataForBookingFlight)),
+      tap(() => this.booked  = true),
+      tap(() => this.flightTemplate = false),
+      tap(() => this.flights = []),
+      catchError(error => this.handleError(error)),
+    ).subscribe();
+  };
+
+
+  // @ts-ignore
+  handleError = (error) => {
+    console.error(error.message);
+    return EMPTY;
   }
 
 }
